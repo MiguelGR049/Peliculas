96df3f50f4f2786ebec548cad063cf6b58fe0a6e
MiguelGR049 <godoyriveramiguel@gmail.com>
2025-05-20 09:36:27 -0600

    Correccion

--- resources/views/pages/agregar.blade.php
+++ resources/views/pages/agregar.blade.php
@@ -5,53 +5,51 @@
 <div class="container mt-5">
     <div class="row mt-5 justify-content-center">
         <div class="col mb-4 text-center">
-            <div class="row mt-5 justify-content-center">
-                <form method="post" action="{{route('insertar')}}" class="col-md-5">
-                    @csrf
-                    <div class="card justify-content-center" style="width: 25rem; background-color: rgba(255, 255, 255, 0.5);">
-                        <img class="mx-auto d-block" src="{{ asset('img/Itadori.png') }}" height="95" width="125px">
-                        <div class="card-body row justify-content-center">
+            <form method="post" action="{{route('insertar')}}" class="row mt-5 justify-content-center">
+                @csrf
+                <div class="card justify-content-center" style="width: 25rem; background-color: rgba(255, 255, 255, 0.5);">
+                    <img class="mx-auto d-block" src="{{ asset('img/Itadori.png') }}" height="95" width="125px">
+                    <div class="card-body row justify-content-center">
 
-                            <h1 class="fw-bold text-center">Agregar</h1>
+                        <h1 class="fw-bold text-center">Agregar</h1>
 
-                            <label class="fw-bold" for="titulo">Titulo</label>
-                            <input name="titulo" id="titulo" class="form-control mb-3" type="text" placeholder="Titulo">
-                            @error('titulo')
-                            <p>{{$message}}</p>
-                            @enderror
+                        <label class="fw-bold" for="titulo">Titulo</label>
+                        <input name="titulo" id="titulo" class="form-control mb-3" type="text" placeholder="Titulo" value="{{old('titulo')}}">
+                        @error('titulo')
+                        <p>{{$message}}</p>
+                        @enderror
 
-                            <label class="fw-bold" for="descripcion">Descripcion</label>
-                            <input name="descripcion" id="descripcion" class="form-control mb-3" type="text" placeholder="Descripcion">
-                            @error('descripcion')
-                            <p>{{$message}}</p>
-                            @enderror
+                        <label class="fw-bold" for="descripcion">Descripcion</label>
+                        <input name="descripcion" id="descripcion" class="form-control mb-3" type="text" placeholder="Descripcion" value="{{old('descripcion')}}">
+                        @error('descripcion')
+                        <p>{{$message}}</p>
+                        @enderror
 
-                            <label class="fw-bold" for="genero">Genero</label>
-                            <input name="genero" id="genero" class="form-control mb-3" type="text" placeholder="Genero">
-                            @error('genero')
-                            <p>{{$message}}</p>
-                            @enderror
+                        <label class="fw-bold" for="genero">Genero</label>
+                        <input name="genero" id="genero" class="form-control mb-3" type="text" placeholder="Genero" value="{{old('genero')}}">
+                        @error('genero')
+                        <p>{{$message}}</p>
+                        @enderror
 
-                            <label class="fw-bold" for="director">Director</label>
-                            <input name="director" id="director" class="form-control mb-3" type="text" placeholder="Director">
-                            @error('director')
-                            <p>{{$message}}</p>
-                            @enderror
+                        <label class="fw-bold" for="director">Director</label>
+                        <input name="director" id="director" class="form-control mb-3" type="text" placeholder="Director" value="{{old('director')}}">
+                        @error('director')
+                        <p>{{$message}}</p>
+                        @enderror
 
-                            <label class="fw-bold" for="fecha_estreno">Fecha Estreno</label>
-                            <input name="fecha_estreno" id="fecha_estreno" class="form-control mb-3" type="date" placeholder="">
-                            @error('fecha_estreno')
-                            <p>{{$message}}</p>
-                            @enderror
+                        <label class="fw-bold" for="fecha_estreno">Fecha Estreno</label>
+                        <input name="fecha_estreno" id="fecha_estreno" class="form-control mb-3" type="date" placeholder="" value="{{old('fecha_estreno')}}">
+                        @error('fecha_estreno')
+                        <p>{{$message}}</p>
+                        @enderror
 
-                            <div class="col justify-content-center text-center">
-                                <button type="submit" class="btn btn-success mb-2">Agregar</button>
-                                <br>
-                            </div>
+                        <div class="col justify-content-center text-center">
+                            <button type="submit" class="btn btn-success mb-2">Agregar</button>
+                            <br>
                         </div>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
